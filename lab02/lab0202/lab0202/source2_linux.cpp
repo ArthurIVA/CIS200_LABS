0e88d8b640e9b45e8ed3e6d65b466843918de40a
Arthur Aigeltinger IV <submail.arthuriva@gmail.com>
2018-09-27 00:07:17 -0400

    Finalized 0201 0202

---
+++ lab02/lab0202/lab0202/source2_linux.cpp
@@ -0,0 +1,74 @@
+//Program Name: Monthly Sales Calculator
+//Programmer Name: Arthur Aigeltinger IV
+//Description: Parses three dimensional array of sales data and computes totals for given departments, stores, and totals. Then writes this data to a formatted table.
+//Date Created: 09/25/18
+
+#include <cctype>
+#include <iomanip>
+#include <iostream>
+
+#define NUM_DEPTS 2
+#define NUM_STORES 2
+#define NUM_MONTHS 12
+
+//Function Prototypes
+void printMonthlySales(float[][NUM_STORES][NUM_MONTHS], int);
+
+
+int main()
+{
+	//Initialize default array
+	float storeMonthlySales[NUM_DEPTS][NUM_STORES][NUM_MONTHS] =
+	{
+		1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.9, 2.0, 2.1, 2.2,
+		2.1, 2.2, 2.3, 2.4, 2.5, 2.6, 2.7, 2.8, 2.9, 3.0, 3.1, 3.2,
+		3.1, 3.2, 3.3, 3.4, 3.5, 3.6, 3.7, 3.8, 3.9, 4.0, 4.1, 4.2,
+		2.1, 2.2, 2.3, 2.4, 2.5, 2.6, 2.7, 2.8, 2.9, 3.0, 3.1, 3.2
+	};
+	//Define and Initialize userChoice
+	int userMonth = 0;
+	char userChoice = ' ';
+
+	do
+	{
+		//Choose month
+		do 
+		{
+			std::cout << "Please Select Month to View (1-12) : ";
+			std::cin >> userMonth;
+		} while (userMonth < 1 || userMonth > 12);
+
+		//Run function while altering user choice month to base zero index.
+		printMonthlySales(storeMonthlySales, userMonth - 1);
+		
+		//Choose to continue or not
+		do
+		{ 
+			std::cout << "Would you like to view another month? (y)es/(n)o?" << std::endl;
+			std::cin >> userChoice;
+			userChoice = std::tolower(userChoice);
+		} while (!(userChoice == 'y' || userChoice == 'n'));
+
+	} while (userChoice != 'n');
+
+	return 0;
+}
+
+//Description: Identifies even integers within a 2 dimensional array and counts how many exist.
+//Pre-Condition: Having array filled with sales data for each given department, store, and month.
+//Post-Condition: n/a
+void printMonthlySales(float storeMonthlySales[][NUM_STORES][NUM_MONTHS], int userMonth)
+{
+	//Calculate row and column additions on the fly
+	float rowOne = storeMonthlySales[0][0][userMonth] + storeMonthlySales[1][0][userMonth];
+	float rowTwo = storeMonthlySales[0][1][userMonth] + storeMonthlySales[1][1][userMonth];
+	float colOne = storeMonthlySales[0][0][userMonth] + storeMonthlySales[0][1][userMonth];
+	float colTwo = storeMonthlySales[1][0][userMonth] + storeMonthlySales[1][1][userMonth];
+	float totalOverall = storeMonthlySales[0][0][userMonth] + storeMonthlySales[0][1][userMonth] + storeMonthlySales[1][0][userMonth] + storeMonthlySales[1][1][userMonth];
+
+	//Formatting and printing table with values.
+	std::cout << "            " << "Dept #1 " << "Dept #2 " << "Store Total" << std::endl;
+	std::cout << "Store #1    " << std::setw(8) << std::left << storeMonthlySales[0][0][userMonth] << std::setw(8) << std::left << storeMonthlySales[1][0][userMonth] << rowOne << std::endl;
+	std::cout << "Store #2    " << std::setw(8) << std::left << storeMonthlySales[0][1][userMonth] << std::setw(8) << std::left << storeMonthlySales[1][1][userMonth] << rowTwo << std::endl;
+	std::cout << "Dept Total  " << std::setw(8) << std::left << colOne << std::setw(8) << std::left << colTwo << totalOverall << std::endl;
+}
---
+++ lab02/lab02_01/lab02_01/source1_linux.cpp
@@ -0,0 +1,122 @@
+//Program Name: 
+//Programmer Name: Arthur Aigeltinger IV
+//Description:
+//Date Created: 09/25/18
+
+#include <cctype>
+#include <iostream>
+#include <iomanip>
+
+#define MAX_ROWS 3
+#define MAX_COULMNS 2
+
+//Function Prototypes
+int howManyEven(int[][MAX_COULMNS]);
+void printArray(int[][MAX_COULMNS]);
+
+
+int main()
+{
+	//Initialize default array contents and 
+	int arrayIntValues[MAX_ROWS][MAX_COULMNS] = { {3,2}, {4,5}, {2,2} };
+	int userIn = 0; //NO LONGER NECESSARY
+	char userChoice = ' ';
+
+	std::cout << "This is the default example of a two-dimensional array." << std::endl;
+	printArray(arrayIntValues);
+
+	std::cout << "The number of even integers in this array is " << howManyEven(arrayIntValues) << std::endl << std::endl;
+
+	std::cout << "Now it's your turn!" << std::endl;
+
+	//Begin menu for user input
+	do 
+	{
+		userChoice = ' ';
+
+		std::cout << "Please enter your own positive integers into the array," << std::endl;
+		std::cout << "Press ENTER / RETURN after each integer." << std::endl;
+
+		//Outer loop iterates through rows with formatting.
+		for (int i = 0; i < MAX_ROWS; i++)
+		{
+			//Inner Loop iterates through sub-columns of array.
+			for (int j = 0; j < MAX_COULMNS; j++)
+			{
+				do
+				{
+					userIn = 0;
+					std::cin >> userIn;
+					
+					if (userIn <= 0)
+					{
+						std::cout << "That was negative... try again you silly goose!" << std::endl;
+					}
+				} while (userIn <= 0);
+				
+				arrayIntValues[i][j] = userIn;
+			}
+		}
+
+		std::cout << "This is your new array!" << std::endl;
+		printArray(arrayIntValues);
+		std::cout << "The number of even integers in this array is " << howManyEven(arrayIntValues) << std::endl << std::endl;
+
+		//Choose to continue or not
+		do
+		{
+			std::cout << "Would you like to enter new integers? (y)es/(n)o?" << std::endl;
+			std::cin >> userChoice;
+			userChoice = std::tolower(userChoice);
+		} while (!(userChoice == 'y' || userChoice == 'n'));
+
+	} while (userChoice != 'n');
+	
+	return 0;
+}
+
+
+//Description: Identifies even integers within a 2 dimensional array and counts how many exist.
+//Pre-Condition: Array that contains integers that can be evaluated as even or odd.
+//Post-Condition: Gives nunber of even integers in the given.
+int howManyEven(int arrayIntValues[][MAX_COULMNS])
+{
+	int numEvens = 0;
+
+	//Outer loop iterates through rows with formatting.
+	for (int i = 0; i < MAX_ROWS; i++)
+	{
+		//Inner Loop iterates through sub-columns of array.
+		for (int j = 0; j < MAX_COULMNS; j++)
+		{
+			if ((arrayIntValues[i][j] % 2) == 0)
+			{
+				numEvens++;
+			}
+		}
+	}
+	return numEvens;
+}
+
+//Description: Iterates through two dimensional array and prints it in a formatted state.
+//Pre-Condition: Have filled array that can be printed.
+//Post-Condition: Formatted array printed for the user.
+void printArray(int arrayIntValues[][MAX_COULMNS])
+{
+	std::cout << std::endl;
+	//Outer loop iterates through rows with formatting.
+	for (int i = 0; i < MAX_ROWS; i++)
+	{
+		//Formatting
+		std::cout << std::setw(4) << std::left;
+
+		//Inner Loop iterates through sub-columns of array.
+		for (int j = 0; j < MAX_COULMNS; j++)
+		{
+			std::cout << " " << arrayIntValues[i][j];
+		}
+		//Drop to next row.
+		std::cout << std::endl;
+	}
+	std::cout << std::endl;
+}
