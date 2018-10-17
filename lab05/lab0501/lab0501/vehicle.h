afcf617dd3de63cc11da420b9e13f25f2c363074
Arthur Aigeltinger IV <submail.arthuriva@gmail.com>
2018-10-17 16:42:49 -0400

    Create 0501

---
+++ lab05/lab0501/lab0501/vehicle.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#ifndef VEHICLE_H
+#define VEHICLE_H
+
+class vehicle
+{
+public:
+
+	vehicle();		//Default Constructor Primitive
+
+	void setAge(int);
+	void setPrice(float);
+
+	int getAge();
+	float getPrice();
+
+	~vehicle();		//Default Constrcutor Primitive
+
+private:
+
+	int age;		//Age of the vehicle
+	float price;	//Price of the vehicle
+};
+
+#endif
