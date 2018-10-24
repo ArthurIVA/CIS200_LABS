dafecc0287e14e1538ba9c3ceea3518bc3b1de64
ArthurIVA <submail.arthuriva@gmail.com>
2018-10-23 21:10:08 -0400

    Began 0602

---
+++ lab06/0602/0602/source.cpp
@@ -0,0 +1,14 @@
+//Program Name: FACTORIAL
+//Programmer Name: Arthur Aigeltinger IV
+//Description: Use recursion to perform the FACTORIAL !
+//Date Created: 10/23/18
+
+#include <iostream>
+
+int main()
+{
+
+
+	system("pause");
+	return 0;
+}
