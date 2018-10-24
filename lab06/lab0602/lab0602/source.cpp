1782cb2199d24f7eece68a8e37504f315c728a74
Arthur Aigeltinger IV <submail.arthuriva@gmail.com>
2018-10-23 22:14:25 -0400

    Finished Base 0602

--- lab06/lab0602/lab0602/source.cpp
+++ lab06/lab0602/lab0602/source.cpp
@@ -5,10 +5,40 @@
 
 #include <iostream>
 
+//Function Prototypes
+int factorial(int input, bool &methodStatus);
+
 int main()
 {
+	//Declare Variables
+	bool methodStatus = false;
+	int input = 0;
+
+	std::cout << "Please enter an integer to find the factorial of: ";
+	std::cin >> input;
+
+	std::cout << input << "! = " << factorial(input, methodStatus) << std::endl;
 
 
 	system("pause");
 	return 0;
+}
+
+int factorial(int input, bool & methodStatus)
+{
+	methodStatus = true;
+
+	if (input < 0)
+	{
+		methodStatus = false;
+		return -1;
+	}
+	else if (input == 0)
+	{
+		return 1;
+	}
+	else
+	{
+		return (input * factorial(input - 1, methodStatus));
+	}
 }
